1e735d1fb2cca61e0498dd7b5681277e051fedc0
Atwau <atwaup7@gmail.com>
2026-02-25 19:54:23 +0300

    building variant calling script

--- variantcalling/script.sh
+++ variantcalling/script.sh
@@ -28,3 +28,28 @@ multiqc qc_reports/ # to combine multiple qc reports
 # application to open files, it automatically chooses which apllication to launch
 # based on the file type you have asked it to open
 xdg-open P7741_R2.fastq.html
+
+# perform trimming to improve read quality
+# for this trimming I will use sickle
+sickle pe -f P7741_R1.fastq.gz -r P7741_R2.fastq.gz -t sanger -q 20 -l 20 -g -o trimmed_R1.fastq.gz -p trimmed_R2.fastq.gz -s trimmed_S.fastq.gz
+
+# next we will perfomr genome mapping
+# aligning the sample genome to the reference genome
+# but first we index the reference genome
+
+# indexing referece genome
+bwa index refgenome/agy99refgenome.fasta
+
+# next we perform the alignment/mapping
+bwa -t 8 refgenome/agy99refgenome.fasta trimmed_R1.fastq.gz trimmed_R2.fastq.gz > output.sam
+
+# convert .sam to .bam
+samtools view -S -b output.sam > output.bam
+
+# after we have to sort the bam file
+samtools sort -o output.sorted.bam output.bam
+
+# we can now generate the mapping statistics to know how well the genome
+# was mapped to the reference
+# for this we se samtools still
+samtools flagstats output.sorted.bam
